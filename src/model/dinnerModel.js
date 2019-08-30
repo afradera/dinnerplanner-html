3b63d010e8b78fbdc70ba96108f4f50e300056bc
Fradera Alicia <af084@hdm-stuttgart.de>
2019-08-30 11:58:01 +0200

    almost finished Lab0

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -3,50 +3,87 @@ class DinnerModel {
 
   constructor() {
     this.dishes = dishesConst;
-
+    //console.log(this.dishes);
     //TODO Lab 0
     // implement the data structure that will hold number of guests
     // and selected dishes for the dinner menu
-
+    this.nrGuests = 0;
+    this.menuDishes = [];
   }
-
+    
   setNumberOfGuests(num) {
-    //TODO Lab 0
+      //TODO Lab 0
+      if (num >= 0) {
+          this.nrGuests = num;
+      } 
   }
 
   getNumberOfGuests() {
     //TODO Lab 0
+    return this.nrGuests;
   }
 
   //Returns the dish that is on the menu for selected type 
   getSelectedDish(type) {
-    //TODO Lab 0
+      for (const dish of this.menuDishes) {
+          if (dish.type == type)
+              return dish; 
+      }
   }
 
   //Returns all the dishes on the menu.
   getFullMenu() {
     //TODO Lab 0
+      return this.menuDishes;
   }
 
   //Returns all ingredients for all the dishes on the menu.
   getAllIngredients() {
     //TODO Lab 0
+    let allIngredients = [];
+    for (const ing of this.menuDishes) { 
+        allIngredients.push(ing.ingredients); 
+        }
+    return allIngredients; 
   }
 
   //Returns the total price of the menu (all the ingredients multiplied by number of guests).
   getTotalMenuPrice() {
     //TODO Lab 0
+    let totalPrice = 0;
+    let ingredientPrice = 0;
+    let ing = this.getAllIngredients();
+
+   for (const pr of ing) {
+        ingredientPrice += pr.price;
+    }
+
+    totalPrice = this.nrGuests * ingredientPrice; 
+    return totalPrice;
   }
 
   //Adds the passed dish to the menu. If the dish of that type already exists on the menu
   //it is removed from the menu and the new one added.
   addDishToMenu(id) {
     //TODO Lab 0 
+    let dsh = this.getDish(id);
+    if (this.getSelectedDish(dsh.type) != undefined){
+        let id = this.getSelectedDish(dsh.type).id;
+        this.removeDishFromMenu(id);
+        this.menuDishes.push(dsh);
+    } else {
+    this.menuDishes.push(dsh);
+    }
   }
 
   //Removes dish from menu
   removeDishFromMenu(id) {
     //TODO Lab 0
+   for(let i = 0; i < this.menuDishes.length; i++){ 
+    if (this.menuDishes[i].id == id) {
+        this.menuDishes.splice(i, 1); 
+    }
+   }
   }
 
 
@@ -54,6 +91,11 @@ class DinnerModel {
   //query argument, text, if passed only returns dishes that contain the query in name or one of the ingredients.
   //if you don't pass any query, all the dishes will be returned
   getAllDishes(type, query) {
+    
+    if (type == undefined && query == undefined) {
+        return this.dishes;
+    } 
+      
     return this.dishes.filter(function (dish) {
       let found = true;
       if (query) {
@@ -67,6 +109,7 @@ class DinnerModel {
           found = true;
         }
       }
+        
       return dish.type === type && found;
     });
   }
